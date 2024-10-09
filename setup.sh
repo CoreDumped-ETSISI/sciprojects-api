4330a486415507fa1b915f08645a23f5097fa824
pandita-IA <panditascan@gmail.com>
2024-10-10 00:26:17 +0200

    [ADD] requirements.txt, setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Solicita al usuario su correo UPM
+read -p "Introduce tu correo UPM: " UPM_EMAIL_ADDRESS
+
+# Solicita al usuario su contraseña UPM (oculta la entrada)
+read -s -p "Introduce tu contraseña UPM: " UPM_EMAIL_PASSWORD
+echo # Esta línea es para un salto de línea después de la contraseña
+
 # Crea un archivo .env para guardar variables de entorno
 echo "Crea un archivo .env para guardar variables de entorno"
-echo "UPM_EMAIL_ADDRESS=tu_correo@upm.es" > .env
-echo "UPM_EMAIL_PASSWORD=tu_contraseña" >> .env
+echo "UPM_EMAIL_ADDRESS='$UPM_EMAIL_ADDRESS'" > .env
+echo "UPM_EMAIL_PASSWORD='$UPM_EMAIL_PASSWORD'" >> .env
 
 # Instala las dependencias de Python
 echo "Instalando dependencias de Python..."
@@ -21,6 +28,10 @@ echo "Navegando al directorio del cliente..."
 cd client || exit
 echo "Instalando dependencias de npm..."
 npm install
+npm install @fortawesome/fontawesome-free
+npm install jwt-decode
+npm install vite
+
 echo "Iniciando el servidor de desarrollo de npm..."
 npm run dev
 
