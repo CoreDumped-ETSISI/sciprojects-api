3f071b15d30eaad51fb73dbfdfd2fedb271c835f
Pandita <113772261+pandita-IA@users.noreply.github.com>
2024-10-10 09:49:57 +0200

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -12,6 +12,32 @@ echo "Crea un archivo .env para guardar variables de entorno"
 echo "UPM_EMAIL_ADDRESS='$UPM_EMAIL_ADDRESS'" > .env
 echo "UPM_EMAIL_PASSWORD='$UPM_EMAIL_PASSWORD'" >> .env
 
+
+# Verifica si Docker está corriendo
+if ! sudo systemctl is-active --quiet docker; then
+    echo "Docker no está corriendo. Iniciando Docker..."
+    sudo systemctl start docker
+fi
+
+# Verifica si el contenedor de MongoDB ya está corriendo
+if [ ! "$(docker ps -q -f name=mongo-db)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=mongo-db)" ]; then
+        # Si el contenedor existe pero está detenido, lo iniciamos
+        echo "Iniciando contenedor de MongoDB existente..."
+        docker start mongo-db
+    else
+        # Si no existe, lo creamos
+        echo "Iniciando nuevo contenedor de Docker para MongoDB..."
+        docker run -d --name mongo-db -p 27017:27017 mongo
+    fi
+else
+    echo "El contenedor de MongoDB ya está corriendo."
+fi
+
+
+echo "MONGO_URI='mongodb://localhost:27017/'" >> .env
+echo "MONGO_DB='WEB_INVESTIGACION'" >> .env
+
 # Instala las dependencias de Python
 echo "Instalando dependencias de Python..."
 pip install -r requirements.txt
